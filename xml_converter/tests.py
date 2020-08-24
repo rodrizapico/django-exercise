43a8e375ba8b258c591c1c5bca7d8f1605269d96
Rodrigo Zapico <rodri.zapico@gmail.com>
2020-08-24 01:41:30 -0300

    added some additional test cases to check for possible errors in implementation

--- xml_converter/tests.py
+++ xml_converter/tests.py
@@ -74,3 +74,121 @@ class XMLConversionTestCase(TestCase):
                 'file',
                 'Invalid file. Please upload a valid XML file.'
             )
+
+    def test_convert_multiple_tags(self):
+        with (TEST_DIR / Path('multiple-tags.xml')).open() as fp:
+            response = self.client.post('/', {
+                'file': fp,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {
+                "PurchaseOrder": [
+                    {
+                        "Address": [
+                              { "Name": "Ellen Adams" },
+                              { "Street": "123 Maple Street" },
+                              { "City": "Mill Valley" },
+                              { "State": "CA" },
+                              { "Zip": "10999" },
+                              { "Country": "USA" }
+                        ]
+                    },
+                    { 
+                        "DeliveryNotes": "Please leave packages in shed by driveway." 
+                    },
+                    { 
+                        "Item": [
+                            { "ProductName": "Lawnmower" },
+                            { "Quantity": "1" },
+                            { "USPrice": "148.95" },
+                            { "Comment": "Confirm this is electric" }
+                        ]
+                    }
+                  ]
+                })
+
+    def test_convert_ignore_attributes(self):
+        with (TEST_DIR / Path('ignore-attributes.xml')).open() as fp:
+            response = self.client.post('/', {
+                'file': fp,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {
+                "PurchaseOrder": [
+                    {
+                        "Address": [
+                              { "Name": "Ellen Adams" },
+                              { "Street": "123 Maple Street" },
+                              { "City": "Mill Valley" },
+                              { "State": "CA" },
+                              { "Zip": "10999" },
+                              { "Country": "USA" }
+                        ]
+                    },
+                    { 
+                        "DeliveryNotes": "Please leave packages in shed by driveway." 
+                    },
+                    { 
+                        "Item": [
+                            { "ProductName": "Lawnmower" },
+                            { "Quantity": "1" },
+                            { "USPrice": "148.95" },
+                            { "Comment": "Confirm this is electric" }
+                        ]
+                    }
+                  ]
+                })
+
+    def test_convert_deep_nesting(self):
+        with (TEST_DIR / Path('deep-nesting.xml')).open() as fp:
+            response = self.client.post('/', {
+                'file': fp,
+            })
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json(), {
+                "PurchaseOrder": [
+                    {
+                        "Address": [
+                              { "Name": "Ellen Adams" },
+                              { "Street": "123 Maple Street" },
+                              { "City": "Mill Valley" },
+                              { "State": "CA" },
+                              { "Zip": "10999" },
+                              { "Country": "USA" }
+                        ]
+                    },
+                    { 
+                        "DeliveryNotes": "Please leave packages in shed by driveway." 
+                    },
+                    { 
+                        "Items": [
+                            {
+                                "Item": [
+                                    { 
+                                        "Product": [
+                                            { "Name": "Lawnmower" },
+                                            { "Description": "The lawnmower everyone deserves!" }
+                                        ] 
+                                    },
+                                    { "Quantity": "1" },
+                                    { "USPrice": "148.95" },
+                                    { "Comment": "Confirm this is electric" }
+                                ]
+                            },
+                            {
+                                "Item": [
+                                    { 
+                                        "Product": [
+                                            { "Name": "Baby Monitor" },
+                                            { "Description": "Keep track of your baby with this baby monitor." }
+                                        ] 
+                                    },
+                                    { "Quantity": "2" },
+                                    { "USPrice": "39.98" },
+                                    { "ShipDate": "1999-05-21" }
+                                ]
+                            }
+                        ]
+                    }
+                  ]
+                })
