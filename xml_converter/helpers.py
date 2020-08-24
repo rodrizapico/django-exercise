72f47a9524a95be489ef34f15479d7174c047442
Rodrigo Zapico <rodri.zapico@gmail.com>
2020-08-24 01:08:26 -0300

    added basic error handling with its corresponding tests

--- xml_converter/helpers.py
+++ xml_converter/helpers.py
@@ -1,10 +1,10 @@
-def recursive_xml_parse(element):
+def xml_to_json(element):
     result = {}
     if len(element) == 0:
         result[element.tag] = element.text or ''
     else:
         result[element.tag] = []
         for child in element:
-            child_result = recursive_xml_parse(child)
+            child_result = xml_to_json(child)
             result[element.tag].append(child_result)
     return result
